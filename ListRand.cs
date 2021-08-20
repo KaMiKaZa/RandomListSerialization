42c576700bfa64d4fd4c42be85b7785bea95313b
KaMiKaZa <kamikaza1997@gmail.com>
2021-08-21 01:37:06 +0500

    Add project files.

---
+++ ListRand.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RandomListSerialization {
+  public class ListNode {
+
+    public ListNode Prev;
+    public ListNode Next;
+
+    /// <summary>
+    /// Произвольный элемент внутри списка 
+    /// </summary>
+    public ListNode Rand;
+
+    public string Data;
+
+    public ListNode() {
+    }
+
+    public ListNode(string data) {
+      Data = data;
+    }
+
+    public override string ToString() {
+      return Data;
+    }
+  }
+
+  public class ListRand {
+    public static readonly Encoding encoding = Encoding.UTF8;
+    public const char endLineSymbol = '\n';
+    public const char inlineDelimiter = '\t';
+
+    /// <summary>
+    /// Узел, являющийся началом списка
+    /// </summary>
+    public ListNode Head;
+
+    /// <summary>
+    /// Узел, являющийся концом списка
+    /// </summary>
+    public ListNode Tail;
+
+    /// <summary>
+    /// Количество узлов в списке
+    /// </summary>
+    public int Count;
+
+    public ListRand() {
+    }
+
+    public ListRand(ListNode head, ListNode tail, int count) {
+      Head = head;
+      Tail = tail;
+      Count = count;
+    }
+
+    private void MapList(Action<ListNode> mapper, ListNode node) {
+      if (node == null) {
+        return;
+      }
+
+      mapper.Invoke(node);
+
+      MapList(mapper, node.Next);
+    }
+
+    public void Serialize(FileStream s) {
+      s.Write(encoding.GetBytes($"{Count}{endLineSymbol}"));
+
+      Dictionary<ListNode, int> randNodeIndexes = new();
+
+      MapList(node => randNodeIndexes.Add(node, randNodeIndexes.Count), Head);
+
+      MapList(node => {
+        int randIndex = (node.Rand == null) ? -1 : randNodeIndexes[node.Rand];
+
+        s.Write(encoding.GetBytes($"{node.Data}{inlineDelimiter}{randIndex}{endLineSymbol}"));
+      }, Head);
+    }
+
+    public void Deserialize(FileStream s) {
+      Dictionary<int, ListNode> indexNodes = new();
+      List<byte> allBytes = new();
+      int intByte;
+
+      while ((intByte = s.ReadByte()) != -1) {
+        allBytes.Add((byte)intByte);
+      }
+
+      List<string> allLines = encoding
+        .GetString(allBytes.ToArray())
+        .Split(endLineSymbol, StringSplitOptions.RemoveEmptyEntries)
+        .Select(line => line.Trim(endLineSymbol))
+        .ToList();
+
+      Count = int.Parse(allLines[0]);
+
+      for (int index = 0; index < Count; index++) {
+        ListNode node = new(allLines[index + 1]);
+
+        indexNodes.Add(index, node);
+
+        if (index == 0) {
+          Head = node;
+        }
+
+        if (index == Count - 1) {
+          Tail = node;
+        }
+      }
+
+      for (int index = 0; index < Count; index++) {
+        ListNode node = indexNodes[index];
+
+        if (index > 0) {
+          node.Prev = indexNodes[index - 1];
+        }
+
+        if (index < Count - 1) {
+          node.Next = indexNodes[index + 1];
+        }
+
+        string[] split = node.Data.Split(inlineDelimiter);
+
+        node.Data = split[0];
+
+        int randIndex = int.Parse(split[1]);
+
+        if (indexNodes.ContainsKey(randIndex)) {
+          node.Rand = indexNodes[randIndex];
+        }
+      }
+    }
+  }
+}
---
+++ ListRandTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RandomListSerialization {
+  public class ListRandTests {
+    public const string SerializeFileName = "ListRandSerialize.txt";
+
+    public static List<ListNode> NodeList;
+    public static ListRand RandomList;
+
+    [SetUp]
+    public void SetUp() {
+      NodeList = new();
+
+      Random random = new Random();
+
+      // диапазон [3, ?) гарантирует, что между head и tail будет как минимум один узел
+      int randomCount = random.Next(3, 20);
+
+      // создаём двусвязный список узлов
+      for (int i = 0; i < randomCount; i++) {
+        if (i == 0) {
+          NodeList.Add(new ListNode("head"));
+        } else {
+          // ставим Data в значение "tail" для последнего узла
+          NodeList.Add(new ListNode((i == randomCount - 1) ? "tail" : $"node {i}"));
+
+          // связываем текущий узел с предыдущим
+          NodeList[i - 1].Next = NodeList[i];
+          NodeList[i].Prev = NodeList[i - 1];
+        }
+      }
+
+      // устанавливаем случайные элементы узлам кроме первого и последнего
+      for (int i = 1; i < randomCount - 1; i++) {
+        if (random.Next(0, 2) == 1) {
+          NodeList[i].Rand = NodeList[random.Next(0, randomCount)];
+        }
+      }
+
+      RandomList = new ListRand(NodeList[0], NodeList[randomCount - 1], randomCount);
+    }
+
+    [Test]
+    public void TestSerialize() {
+      using (var fileStream = File.OpenWrite(SerializeFileName)) {
+        RandomList.Serialize(fileStream);
+      }
+
+      var serializedFileInfo = new FileInfo(SerializeFileName);
+
+      // проверяем, что файл с сериализацией существует и он не пустой
+      Assert.True(serializedFileInfo.Exists && serializedFileInfo.Length > 0);
+    }
+
+    [Test]
+    public void TestDeserialize() {
+      using (var fileWriteStream = File.OpenWrite(SerializeFileName)) {
+        RandomList.Serialize(fileWriteStream);
+
+        // обнуляем объект, который был создан перед запуском теста
+        RandomList = new();
+      }
+
+      using (var fileReadStream = File.OpenRead(SerializeFileName)) {
+        RandomList.Deserialize(fileReadStream);
+      }
+
+      // проверяем, что процесс Serialize -> Deserialize не испортил значение Count
+      Assert.AreEqual(NodeList.Count, RandomList.Count);
+
+      List<ListNode> deserializedNodes = new();
+      ListNode node = RandomList.Head;
+
+      for (int i = 0; i < RandomList.Count; i++) {
+        deserializedNodes.Add(node);
+
+        node = node.Next;
+      }
+
+      CollectionAssert.AreEqual(
+        deserializedNodes.Select(node => $"{node.Data} + {node.Rand?.Data ?? "null"}"),
+        NodeList.Select(node => $"{node.Data} + {node.Rand?.Data ?? "null"}")
+      );
+    }
+
+    [TearDown]
+    public void TearDown() {
+      NodeList = null;
+      RandomList = null;
+
+      if (File.Exists(SerializeFileName)) {
+        File.Delete(SerializeFileName);
+      }
+    }
+  }
+}
